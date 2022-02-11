94f1f8b9985d5385f7fec4bdeb94ff32572084de
RezaPasand <74759697+rpnd@users.noreply.github.com>
2022-02-11 14:24:05 +0330

    Comment you want

--- index.js
+++ index.js
@@ -16,4 +16,7 @@ closeBtn.addEventListener("click", () => {
 //change theme
 themeToggler.addEventListener("click", () => {
   document.body.classList.toggle("dark-theme-variables");
+
+  themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+  themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
 });
---
+++ order.js
@@ -0,0 +1,38 @@
+const orders=[
+{
+    productName:'KALBAS',
+    productNumber:'121212',
+    paymentStatus:'Due',
+    shipping:'Pending'
+},
+{
+    productName:'Bastani',
+    productNumber:'878787',
+    paymentStatus:'Due',
+    shipping:'Pending'
+},{
+    productName:'zahremar',
+    productNumber:'363636',
+    paymentStatus:'Due',
+    shipping:'Pending'
+},{
+    productName:'dildo',
+    productNumber:'696969',
+    paymentStatus:'Due',
+    shipping:'Pending'
+},{
+    productName:'Ash ba jash',
+    productNumber:'121212',
+    paymentStatus:'Due',
+    shipping:'Pending'
+},{
+    productName:'KALBAS',
+    productNumber:'121212',
+    paymentStatus:'Due',
+    shipping:'Pending'
+},
+
+
+
+
+]
